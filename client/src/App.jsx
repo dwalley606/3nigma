874ce3562b68ef47db4e3286ef636053e3dcbe94
dwalley606 <dwalley606@gmail.com>
2024-10-10 20:49:06 -0400

    Added client, scaffolding front end

---
+++ client/src/App.jsx
@@ -0,0 +1,14 @@
+   // src/App.jsx
+   import React from 'react';
+   import MessageComponent from './MessageComponent';
+
+   function App() {
+     return (
+       <div className="App">
+         <h1>Vite React Messaging App</h1>
+         <MessageComponent />
+       </div>
+     );
+   }
+
+   export default App;
