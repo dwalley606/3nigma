a0443a84c3ef16487bdfb2cf03fe170f37594a81
Daniel Walley <dwalley606@gmail.com>
2024-12-05 21:37:05 -0500

    Installed typescript dependencies, set up testing library

--- jest.config.js
+++ jest.config.js
@@ -3,13 +3,26 @@ module.exports = {
   projects: [
     {
       displayName: "client",
-      testMatch: ["<rootDir>/client/src/tests/**/*.test.js"], // Ensure this path matches your client test files
+      testMatch: ["<rootDir>/client/src/**/*.test.{ts,tsx,js,jsx}"],
       testEnvironment: "jest-environment-jsdom",
+      transform: {
+        "^.+\\.(ts|tsx)$": "ts-jest",
+        "^.+\\.(js|jsx)$": "babel-jest"
+      },
+      moduleNameMapper: {
+        "^@/(.*)$": "<rootDir>/client/src/$1"
+      }
     },
     {
       displayName: "server",
-      testMatch: ["<rootDir>/server/tests/**/*.test.js"], // Ensure this path matches your server test files
+      testMatch: ["<rootDir>/server/**/*.test.{ts,js}"],
       testEnvironment: "node",
-    },
-  ],
+      transform: {
+        "^.+\\.ts$": "ts-jest"
+      },
+      moduleNameMapper: {
+        "^@/(.*)$": "<rootDir>/server/$1"
+      }
+    }
+  ]
 };
