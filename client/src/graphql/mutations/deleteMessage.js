b4603ff369849237a1156e91cdfddc90c8284fd6
Daniel <dwalley@gmail.com>
2024-12-10 17:02:09 -0500

    GraphQL folder is now all .graphql, need to run codegen and update imports

--- client/src/graphql/mutations/deleteMessage.js
+++
@@ -1,7 +0,0 @@
-import { gql } from "@apollo/client";
-
-export const DELETE_MESSAGE= gql`
-  mutation DeleteMessage($messageId: ID!, $forEveryone: Boolean!) {
-    deleteMessage(messageId: $messageId, forEveryone: $forEveryone)
-  }
-`;
--- client/src/graphql/mutations/markMessagesAsRead.js
+++
@@ -1,8 +0,0 @@
-// client/src/graphql/mutations/markMessagesAsRead.js
-import { gql } from "@apollo/client";
-
-export const MARK_MESSAGES_AS_READ = gql`
-  mutation MarkMessagesAsRead($conversationId: ID!) {
-    markMessagesAsRead(conversationId: $conversationId)
-  }
-`;
